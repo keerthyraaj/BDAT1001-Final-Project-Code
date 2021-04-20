5bcdb8f45f52b039efe54f5e10c6a28caf9fc038
keerthyraaj <60767880+keerthyraaj@users.noreply.github.com>
2021-04-21 00:23:27 +0530

    Add files via upload

---
+++ video - 7  (ep.5)/Server/Controller/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        public IActionResult Authenticate()
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
