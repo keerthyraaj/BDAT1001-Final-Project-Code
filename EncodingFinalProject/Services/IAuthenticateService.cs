5716b0b44c8c31716d85e4f569d1a1716f0f04b0
keerthyraaj <60767880+keerthyraaj@users.noreply.github.com>
2021-04-21 04:20:15 +0530

    Add files via upload

---
+++ EncodingFinalProject/Services/IAuthenticateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EncodingFinalProject.Models;
+
+namespace EncodingFinalProject.Services
+{
+    public interface IAuthenticateService
+    {
+        Teacher Authenticate(String UserName, String Password);
+    }
+
+}
